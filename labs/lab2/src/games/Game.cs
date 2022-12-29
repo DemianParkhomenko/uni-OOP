3276979485a57658f4aa42a398f0f7c0a16ec7b6
Demian Parkhomenko <parkhomenko785@gmail.com>
2022-12-29 19:52:43 +0200

    Add generic input to base Game

--- labs/lab2/src/games/Game.cs
+++ labs/lab2/src/games/Game.cs
@@ -1,28 +1,70 @@
 namespace Lab2;
+
+public delegate bool ValidateGetFromPlayer<T>(T input);
+
 public abstract class Game
+{
+  protected Random random = new Random();
+
+  abstract public void Play(Account account1, Account account2,
+  BalanceTypes balanceType, decimal points);
+
+  virtual protected void rewardPlayers(BalanceTypes balanceType, decimal points,
+                  Account winner, Account loser)
   {
-    protected Random random = new Random();
-
-    abstract public void Play(Account account1, Account account2,
-    BalanceTypes balanceType, decimal points);
-
-    virtual protected void rewardPlayers(BalanceTypes balanceType, decimal points,
-                    Account winner, Account loser)
+    try
     {
-      try
-      {
-        winner.onWin(balanceType, points, loser.Name);
-        loser.onLose(balanceType, points, winner.Name);
-      }
-      catch (Exception ex)
-      {
-        Console.WriteLine($"Exception on rewarding players", ex);
-      }
+      winner.onWin(balanceType, points, loser.Name);
+      loser.onLose(balanceType, points, winner.Name);
     }
-
-    protected bool randomBool()
+    catch (Exception ex)
     {
-      return random.Next(2) == 0;
+      Console.WriteLine($"Exception on rewarding players", ex);
     }
   }
+
+  protected bool randomBool()
+  {
+    return random.Next(2) == 0;
+  }
+
+  protected void printWinnerLoser(Account winner, Account loser)
+  {
+    Console.WriteLine($"Winner: {winner.Name}({winner.Email})."
+       + $" Loser: {loser.Name}({loser.Email})");
+  }
+
+  void writeTryAgainMessage()
+  {
+    Console.WriteLine("❌ Invalid input. Let's try again...");
+  }
+
+  void askFor(string whatToEnter, Account player)
+  {
+    Console.WriteLine($"{player.Name} enter " + whatToEnter + "...");
+  }
+
+  T getFromPlayer<T>(Account account, ValidateGetFromPlayer<T> validate)
+  {
+    try
+    {
+      string line = Console.ReadLine() ?? "";
+      T input = (T)Convert.ChangeType(line, typeof(T));
+      if (validate(input)) return input;
+      writeTryAgainMessage();
+      return getFromPlayer<T>(account, validate);
+    }
+    catch
+    {
+      writeTryAgainMessage();
+      return getFromPlayer<T>(account, validate);
+    }
+  }
+
+  protected T askAndGetFromPlayer<T>(string whatToEnter, Account player, ValidateGetFromPlayer<T> validate)
+  {
+    askFor(whatToEnter, player);
+    return getFromPlayer<T>(player, validate);
+  }
+}
 
